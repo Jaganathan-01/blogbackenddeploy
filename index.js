89476eb6e21c3bb6b0218b5e1de9cf82d91ee136
Jaganathan Govindhan <100895340+jaganathan20ITL116@users.noreply.github.com>
2022-11-07 14:48:12 +0530

    second

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+var cors = require('cors')
 
 dotenv.config();
 app.use(express.json());
+app.use(cors({origin:"*"}))
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.get('/', (req, res) => {
   res.send("GET Request Called")
